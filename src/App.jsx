8efcc5cbeab5ba10437a95f60372411d478a2ae8
Ahmed-Raza001 <ahmadrazaharsoliya67@gmail.com>
2025-08-23 06:50:05 +0000

    A

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+/*function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -30,10 +30,10 @@ function App() {
       </p>
     </>
   )
-}
+}*/
 
 // App.jsx
-import Greeting from "./Greeting";
+/*import Greeting from "./Greeting";
 
 function Not() {
   return (
@@ -43,4 +43,36 @@ function Not() {
   );
 }
 
-export default Not;
+export default Not;*/
+
+const COLORS = ["pink", "green", "blue", "yellow", "purple", "red"];
+
+function App() {
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      {COLORS.map((color) => (
+        <button
+          type="button"
+          key={color}
+          onClick={onButtonClick(color)}
+          className={backgroundColor === color ? "selected" : ""}
+        >
+          {color}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default App;
